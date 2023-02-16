39afd0c7ef96ff09eed70c272b458207bfd9cae1
lux <f.lux@fz-juelich.de>
2023-02-15 21:40:08 -0500

    Basic file structure

---
+++ src/generate_laplacian.cpp
@@ -0,0 +1,22 @@
+/*
+ * ./src/generate_laplacian.cpp
+ *
+ * Author:  Fabian R. Lux
+ * Date:    02/15/2023
+ *
+ * Read the basis file and construct the Laplacian.
+ *
+ */
+
+int main()
+{
+  // Read the input files
+  
+  // Read the basis file 
+  
+  // Set-up the Laplacians for A,B and AB
+  
+  // Store the Laplacians to file
+
+  return 0;
+}
