8e99f7d3e3d80550a9109fd91294c13e59bdc593
lux <f.lux@fz-juelich.de>
2023-02-21 20:36:17 -0500

    Unsure with inpgen

--- scripts/inpgen.py
+++ scripts/inpgen.py
@@ -8,28 +8,54 @@
     for the execution of the C++ and Fortran programs.
 """
 
-import time
 import os
+import sys
+import time
 
-BUILD_DIRECTORY =  os.environ['HYPERBOLIC_BUILD']
-OUT_DIRECTORY = os.environ.get('HYPERBOLIC_OUT', BUILD_DIRECTORY+"\\out\\")
+BUILD_DIRECTORY = os.environ['HYPERBOLIC_BUILD']
+# OUT_DIRECTORY   = os.environ.get('HYPERBOLIC_OUT', BUILD_DIRECTORY+"\\out\\")
+
+def mkdir(PATH):
+    """
+        Creates a directory at PATH if not already existent
+    """
+    if(os.path.isdir(PATH)):
+        pass
+    else:
+        os.mkdir(PATH)
 
 def set_unique_identifier():
     """
         Use the universal time, to create a unique identifier for the output folder.
     """
+
+    # -- waiting for one second will ensure uniqueness of identifier
     time.sleep(1)
-    
-    HYPERBOLIC_DIR = OUT_DIRECTORY+str(round(time.time()))
-    
-    os.environ['HYPERBOLIC_DIR'] = HYPERBOLIC_DIR 
-    
+
+    OUT_DIR = os.getcwd() + 
+    mkdir(OUT_DIR)
+
+    if sys.platform in ['linux', 'macos']:
+
+        HYPERBOLIC_DIR = OUT_DIR + '/' + str(round(time.time()))
+        mkdir(HYPERBOLIC_DIR)
+
+        # -- create .sh file to export HYPERBOLIC_DIR
+
+        shellfile = HYPERBOLIC_DIR + '/hyperbolic_vars.sh'
+        with open(shellfile,'w') as file:
+            file.write("export HYPERBOLIC_DIR="+HYPERBOLIC_DIR)
+            print(shellfile) 
+
+    elif sys.platorm ['win32', 'cygwin']: 
+        HYPERBOLIC_DIR = ".\\out\\" +str(round(time.time()))
+        
     if(os.path.isdir(HYPERBOLIC_DIR)):
         #-- this should not happen
         pass
     else:
         os.mkdir(HYPERBOLIC_DIR)
-    
+
     print("${HYPERBOLIC_DIR} =", HYPERBOLIC_DIR )
     
 
