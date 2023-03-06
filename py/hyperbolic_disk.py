eb817e2f6c8d653ddf2b6af592fb83170574719d
lux <f.lux@fz-juelich.de>
2023-03-05 19:39:33 -0500

    Better plot

--- py/hyperbolic_disk.py
+++ py/hyperbolic_disk.py
@@ -191,7 +191,7 @@ if __name__=="__main__":
         for j in range(n_phi):
             z = rs[i] * np.exp(1j*phis[j])
             for region in range(3):
-                chis[region,i,j] = smooth_phase(z,0.5,phi=phi,region=region)
+                chis[region,i,j] = smooth_phase(z,0.3,phi=phi,region=region)
 
     fig, axs = plt.subplots(1,3, subplot_kw=dict(projection="polar"))
     deg = 180 / np.pi
@@ -199,15 +199,16 @@ if __name__=="__main__":
     titles = [r"$\chi_1(z)$", r"$\chi_2(z)$" , r"$\chi_3(z)$"]
     for region in range(3):
         ax = axs[region]
-       	ax.set_xticklabels([])
+       	ax.set_xticklabels([r"$\Upsilon_1^\infty$", r"$\Upsilon_2^\infty$", r"$\Upsilon_3^\infty$"])
        	ax.set_yticklabels([])
         ax.set_thetagrids([phi*deg, phi*deg+120, phi*deg+240])
         ax.set_rticks([])
         ax.set_title(titles[region])
         ctf = ax.contourf(phis,rs,chis[region], levels=400, cmap='viridis')
-    plt.tight_layout()
-    
-    sk = 0.4
-    fig.colorbar(ctf, ax=axs.ravel().tolist(), orientation='vertical', ticks=[0,0.5,1], shrink=sk, aspect=20*sk)
-    plt.savefig("Y_junction.png", dpi=300) 
+   
+     
+    plt.tight_layout(w_pad=3)
+    sk = 0.25
+    fig.colorbar(ctf, ax=axs.ravel().tolist(), orientation='vertical', ticks=[0,0.5,1], shrink=sk, aspect=20*sk, pad=0.1)
+    plt.savefig("Y_junction_0.3.png", dpi=300) 
     plt.show()
