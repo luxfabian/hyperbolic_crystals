b4804ed24250604db6554a7d00e560dc9e93f578
Fabian Lux <fabian.lux@yu.edu>
2023-09-21 13:26:13 -0400

    specific io

--- py/tffg_iomodule.py
+++ py/tffg_iomodule.py
@@ -84,6 +84,59 @@ def get_access_point(silent=True):
     return access_point
 
 
+def get_specific_access_point(g,N, silent=True):
+    """
+        Provides a dictionary access_point with access to the file
+        locations of the regular representation and its dimensions.
+        The keywords are:
+
+            - 'g': genus of the Fuchsian group
+            - 'N': finite approximation control parameter
+            - 'd': dimension of the Hilbert space
+            - 'fprefix': the prefix which should be used to ensure uniform file names
+    """
+
+    access_point = {}
+
+    HYPERBOLIC_DIR = os.getenv('HYPERBOLIC_DIR')
+
+    ###################################################################
+    # Read group_specs.inp                                            #
+    ###################################################################
+
+    access_point['g'] = g
+    access_point['N'] = N
+
+    periodic_boundary = not (N < 0)
+
+    fname_prefix = HYPERBOLIC_DIR + "/tffg_" + str(g) + "_"
+
+    if(periodic_boundary):
+        fname_prefix += "modulo_"+str(N)
+    else:
+        fname_prefix += "open_"+str(-N)
+
+    access_point['fprefix'] = fname_prefix
+
+    #######################################################################
+    # Read spectral information                                           #
+    #######################################################################
+
+    spec_info_fname = fname_prefix + ".info"
+    spec_info_file = open(spec_info_fname, 'r')
+    spec_info_lines = spec_info_file.readlines()
+    spec_info_lines = [re.sub("[^0-9]", "", line) for line in spec_info_lines]
+    d = int(spec_info_lines[0])
+
+    access_point['d'] = d
+
+    if not silent:
+        print("Hilber space dimension: ", d)
+
+    spec_info_file.close()
+
+    return access_point
+
 if __name__ == '__main__':
 
     access_point = get_access_point()
